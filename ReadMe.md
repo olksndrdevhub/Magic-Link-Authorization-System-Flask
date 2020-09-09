5d61b266edb6d9d3e6887093579133a2cc71544d
Oleksandr Romaniuk <oleksandr.romaniuk.work@gmail.com>
2020-09-09 19:45:44 +0300

    change magic link and update readme

--- ReadMe.md
+++ ReadMe.md
@@ -1,6 +1,7 @@
-This is Magic Link Authorization System.
+##This is Magic Link Authorization System.
 
 For start you need create and activate virtual environment, then clone this repo.  
+
 After that run:  
 `pip install -r requirements.txt`  
 `flask db init`  
@@ -9,3 +10,28 @@ After that run:
 `flask run`
 
 Server start on <a>localhost:5000</a> so go on this address in your browser.  
+
+###How it works.  
+
+#####I built this project based on Flask.
+I installed and used the following extensions:
+>Flask Mail (for sending letters)
+>Flask SQLAlchemy (for interaction with the database)
+>Flask Migrate (for database interaction)
+
+I also used the built-in python libraries:
+>secrets (for generate token)
+>hashlib (for generate hash)
+
+
+When the main page is opened, the user sees the field and the send button. To register and receive a magic link, the user must enter an email and click button.  
+Then app generates a token and hashcode based on the user's email, saves the user's email, hashcode and token in the database, then generates a magic link consisting of an email hash and a token separated by '_and_'.  
+
+After that, a letter with this magical link is formed and sent to the user's mail. The user is redirected to the page with a request to check his e-mail.  
+When the user clicks on the link in the letter, app check if the hash and token match the existing data in the database. If successful, it increases by one value in the login counter and user is redirected to the profile page. On this page user can see the count of logins and can to log out of the account.  
+
+The user session is based on the `flask.session` object. When the user clicks on the magic link, app sets the new `key:value` pair (`user:user_email`) in the `session` object, which is deleted if the user clicks the logout button. 
+
+There is also a decorator that gives access to the profile page only for authorized users.  
+
+Also I create a test Gmail account to send emails via this app.
--- main.py
+++ main.py
@@ -5,15 +5,17 @@ from flask_mail import Mail, Message
 
 import secrets
 from functools import wraps
+import hashlib
 
 
 
 app = Flask(__name__)
 
+# set secret key for using session
 app.config['SECRET_KEY'] = 'thisismysecretkey'
 
 
-
+# set config values for flask-mail
 app.config.update(dict(
     DEBUG = True,
     MAIL_SERVER = 'smtp.gmail.com',
@@ -25,52 +27,66 @@ app.config.update(dict(
 ))
 mail = Mail(app)
 
+# set database config
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 
-
+# create user model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=True)
+    hash_email = db.Column(db.String(300), unique=True, nullable=True)
     token = db.Column(db.String(200), unique=True, nullable=True)
     log_counter = db.Column(db.Integer, default = 0)
 
     def __repr__(self):
         return '<User: %r>' % self.email
 
-
+# create decorator for prevent unauthorized access
+# it`s check that cyrrent user are authorized or not
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if 'user' not in session:
-        # if CURRENT_USER is None:
             return redirect(url_for('index'))
         return f(*args, **kwargs)
     return decorated_function
 
+
+# index route
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
     error_message = None
     
+    # check if user already log in
     if 'user' in session:
         u = User.query.filter_by(email=session['user']).first()
         return redirect(url_for('profile', cur_user_id=u.id))
 
+    # when someone type in input and press send button
     if request.method == 'POST':
         email = request.form['email']
+
+        # check if this email already was registerd  
         if not User.query.filter_by(email=email).first():
+            # generate token and hash email
             token = secrets.token_urlsafe()
+            hash_obj = hashlib.md5(email.encode())
+            md5_hash = hash_obj.hexdigest()
             
-            user = User(email=email, token=token)
+            # create user
+            user = User(email=email, token=token, hash_email=md5_hash)
             db.session.add(user)
             db.session.commit()
 
-            login_link = str(request.base_url+'_login_request/user_{}/token_{}'.format(user.email, token))
+            # generate magic link
+            login_link = str(request.base_url+'_login_request/{}_and_{}'.format(user.hash_email, token))
 
+            # setup, generate and send mail to inputed email
             mail_message = Message('Hi, this is your link to log in', 
                                     sender=app.config['MAIL_USERNAME'],
                                     recipients=[email])
@@ -79,35 +95,40 @@ def index():
             print('message sended to email: {}'.format(email))
             
             return render_template('confirm.html')
-        
+        # if inputed email already used user see this error
         error_message = 'This email already register! Use another one!'
         
     return render_template('index.html', error_message=error_message)
 
+
+# route for magic link, when user go via sended for him magic link
+# in this part we verify link, increase counter, that count ho much times user log in and than redirect to profile
 @login_required
-@app.route('/_login_request/user_<email>/token_<token>')
-def login_request(email, token):
-    cur_user = User.query.filter_by(email=email, token=token).first()
+@app.route('/_login_request/<hash_email>_and_<token>')
+def login_request(hash_email, token):
+    cur_user = User.query.filter_by(hash_email=hash_email, token=token).first()
     if cur_user:
+        # increace counter
         cur_user.log_counter = cur_user.log_counter + 1
         db.session.commit()
-
+        # setup current session user
         session['user'] = cur_user.email
-        
+        # redirect to profile
         return redirect(url_for('profile', cur_user_id=cur_user.id))
+    # return error if link not verify
+    return 'loaded with some error, not sign in...'
 
-    return 'loaded, but not sign in...'
-
+# logout route
 @app.route('/logout')
 def logout():
 
+    # clear current session
     session.pop('user', None)
     
     return redirect(url_for('index'))
 
 
-
-
+# user profile route. this page visible only after user tap on magic link.
 @app.route('/profile/<cur_user_id>')
 @login_required
 def profile(cur_user_id):
@@ -119,11 +140,18 @@ def profile(cur_user_id):
 
 
 
-@app.route('/delete_users/')
+
+# this part - route for quick delete all users, drop database and create again
+
+@app.route('/del/delete_users/')
 def delete_users():
     users = User.query.all()
     for user in users:
         db.session.delete(user)
+        db.session.commit()
+        db.drop_all()
+        db.session.commit()
+        db.create_all()
         db.session.commit()
     return 'users deleted'
 
